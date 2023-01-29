55733e0f0e1c703c8a492265190203a8df80f3e7
omar mohamed <omer.eldanin@gmail.com>
2023-01-29 03:20:46 +0200

    finish all

--- js/main.js
+++ js/main.js
@@ -11,12 +11,53 @@ let bars = document.querySelector("nav .fa-bars"),
   startVideoButtons = document.querySelectorAll(".video img"),
   videos = document.querySelectorAll("video"),
   mainLoading = document.querySelector(".loading"),
-  companyNumbers = document.querySelectorAll(".our-number .col div .number");
+  companyNumbers = document.querySelectorAll(".our-number .col div .number"),
+  navbar = document.querySelector("nav"),
+  lang = document.querySelector("nav .lang"),
+  filters = document.querySelectorAll("section .filters a"),
+  containers = document.querySelectorAll("section .worksContainer .row"),
+  startAudiobuttons = document.querySelectorAll(".audio i"),
+  audios = document.querySelectorAll("audio");
 
-window.onload = () => {
-  mainLoading.classList.remove("start-loading");
+lang.onclick = (e) => {
+  const location = document.location.pathname;
+  if (location.includes("/en")) {
+    document.location.assign(`${location.slice(3)}`);
+    console.log(location.slice(3));
+  } else {
+    document.location.assign(`en${location}`);
+  }
 };
+window.onload = () => {
+  if (mainLoading) {
+    mainLoading.classList.remove("start-loading");
+  }
+};
+if (filters) {
+  filters.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      filters.forEach((ele) => {
+        ele.classList.remove("active");
+      });
+      item.classList.add("active");
+      const containerId = item.dataset.work;
+      containers.forEach((container) => {
+        container.classList.remove("open");
+      });
+      containers.forEach((container) => {
+        if (container.id === containerId) {
+          container.classList.add("open");
+        }
+      });
+    });
+  });
+}
 window.onscroll = () => {
+  if (window.pageYOffset > 100) {
+    navbar.classList.add("fixedTop");
+  } else {
+    navbar.classList.remove("fixedTop");
+  }
   if (window.pageYOffset > 250) {
     companyNumbers.forEach((item) => {
       const number = item.dataset.number;
@@ -32,42 +73,9 @@ window.onscroll = () => {
     });
   }
 };
-var swiper = new Swiper(".mySwiper", {
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    300: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    600: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  },
-});
-
-var swiper2 = new Swiper(".mySwiper2", {
-  spaceBetween: 50,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
 
 bars.onclick = (e) => {
-  navLinks_ul.classList.toggle("open-ul");
+  navbar.classList.toggle("open");
   bars.classList.toggle("fa-times");
 };
 
@@ -97,6 +105,45 @@ videos.forEach((item) => {
   });
 });
 
+startAudiobuttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    audios.forEach((audio) => {
+      audio.pause();
+    });
+    const parent = e.target.parentNode;
+    let audio = parent.children[0];
+    if (e.target.classList.value.includes("fa-play")) {
+      e.target.classList.remove("fa-play");
+      e.target.classList.add("fa-pause");
+      audio.play();
+    } else {
+      e.target.classList.add("fa-play");
+      e.target.classList.remove("fa-pause");
+      audio.pause();
+    }
+    audio.onended = () => {
+      e.target.classList.add("fa-play");
+      e.target.classList.remove("fa-pause");
+    };
+  });
+});
+audios.forEach((item) => {
+  item.addEventListener("pause", (e) => {
+    const parent = item.parentNode;
+    const pauseButton = parent.children[2];
+    pauseButton.classList.add("fa-play");
+    pauseButton.classList.remove("fa-pause");
+  });
+});
+audios.forEach((audio) => {
+  audio.addEventListener("timeupdate", (e) => {
+    const parent = audio.parentNode.parentNode.parentNode;
+    const progress1 = parent.children[0].children[0];
+    const progress2 = parent.children[2].children[0];
+    progress1.setAttribute("value", audio.currentTime / audio.duration);
+    progress2.setAttribute("value", audio.currentTime / audio.duration);
+  });
+});
 const addEventsToInputs = (inputs) => {
   inputs.forEach((item) => {
     item.addEventListener("focus", (e) => {
@@ -120,16 +167,3 @@ addEventsToInputs(contactUsInputs);
 formContainer.onclick = (e) => {
   e.stopPropagation();
 };
-var wavesurfer = WaveSurfer.create({
-  container: "#waveform",
-  progressColor: "#00D0B0",
-  waveColor: "#B8BABF",
-  barWidth: 1,
-  barRadius: 1,
-  barGap: 4,
-  height: 150,
-  cursorWidth: 0,
-  fillParent: true,
-  interact: false,
-});
-wavesurfer.load("../images/audio.mp3");
